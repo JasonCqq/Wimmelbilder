5130ee74a5974bfb3d48cef41f824062f7402902
Jason <121495300+jason21715@users.noreply.github.com>
2023-05-30 07:51:23 -0700

    Build Deployment #2

---
+++ src/Firebase.js
@@ -0,0 +1,62 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  doc,
+  setDoc,
+  getDoc,
+  query,
+  where,
+  getDocs,
+  updateDoc,
+  addDoc,
+  collectionGroup,
+} from "firebase/firestore";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+import Config from "./Firebaseconfig.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: Config.API_KEY,
+  authDomain: Config.AUTH_DOMAIN,
+  projectId: Config.PROJECT_ID,
+  storageBucket: Config.STORAGE_BUCKET,
+  messagingSenderId: Config.MESSAGING_SENDER_ID,
+  appId: Config.APP_ID,
+  measurementId: Config.MESSAGING_SENDER_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  getFirestore,
+  collection,
+  doc,
+  setDoc,
+  getDoc,
+  getDocs,
+  addDoc,
+  app,
+  onAuthStateChanged,
+  query,
+  where,
+  updateDoc,
+  collectionGroup,
+};
